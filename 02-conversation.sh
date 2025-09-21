1e05771251923c06998956909f81eafa1a3cb487
unknown <tirusatrapu36@gmail.com>
2025-09-21 10:20:48 +0530

    some massage

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hi Suresh, How are you?"
+echo "Suresh:: Hello Reamesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shall scripting Ramesh"
