09a574b6c2acbe69fc3dee3e114602d9ecdfc45a
unknown <tirusatrapu36@gmail.com>
2025-09-21 10:28:52 +0530

    some massage

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh:: hi Suresh, How are you?"
-echo "Suresh:: Hello Reamesh, I am fine. How are you?"
-echo "Ramesh:: What are you learning recently?"
-echo "Suresh:: I am learning Shall scripting Ramesh"
+#no spaces for varibules
+person1=Malli
+person2=Nani
+
+echo "$person1:: hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: What are you learning recently?"
+echo "$person2:: I am learning Shall scripting $person1"
