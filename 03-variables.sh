089fc51c15b3e7b883b194cd37fad8343ef6eaca
unknown <tirusatrapu36@gmail.com>
2025-09-22 11:22:54 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,4 +7,4 @@ person2=$2
 echo "$person1:: hi $person2, How are you?"
 echo "$person2:: Hello $person1, I am fine. How are you?"
 echo "$person1:: What are you learning recently?"
-echo "$person2:: I am learning Shall scripting $person1"0890-
+echo "$person2:: I am learning Shall scripting $person1"
