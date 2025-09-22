409233a0aa917db48ce171973c9bd8d81e4d2d54
unknown <tirusatrapu36@gmail.com>
2025-09-22 11:17:25 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no spaces for varibules
+person1=$1
+person2=$2
+
+echo "$person1:: hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: What are you learning recently?"
+echo "$person2:: I am learning Shall scripting $person1"0890-
