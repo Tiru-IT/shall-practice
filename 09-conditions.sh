ffd00306dd859a314d235ba63b2e4cb921c50345
unknown <tirusatrapu36@gmail.com>
2025-09-22 21:13:18 +0530

    spcial variables

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ NUMBER -lt 10 ]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given number is $NUMBER is less then 10"
 else
     echo "Given number is $NUMBER is greater than or equal to 10"
