224d24241a2a4d7f1b9bcb88a665caba2f1d1699
unknown <tirusatrapu36@gmail.com>
2025-09-22 21:01:51 +0530

    spcial variables

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -8,3 +8,10 @@ NUMBER2=200
 sum=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is: ${sum}"
+
+# arrays 
+
+LEARDES=("MODI" "PUTIN" "TRUMP")
+echo "All leardes is: ${LEARDES[@]}"   
+echo "fIRST leardes is: ${LEARDES[0]}"   
+echo "Second leardes is: ${LEARDES[1]}"   
